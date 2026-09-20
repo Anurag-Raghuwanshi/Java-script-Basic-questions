d3d9865cd93ae44c6916fad7b478e17417991c0d
Anurag Raghuwanshi <anuragraghuwanshi077@gmail.com>
2026-09-20 12:05:58 +0530

    Assignment

---
+++ Assignmennt.js
@@ -0,0 +1,40 @@
+          //Question no = 1;
+let arr = [7, 9,0,-2];
+let n = prompt("Enter the lenght of array you have to find");
+console.log(arr.slice(0,n));
+
+         //Question no = 2;
+let array = [7,9,0,-2];
+let num = prompt("Enter the lenght of array you have to find");
+ console.log(array.slice(array.length-num));
+
+        //Question no = 3;
+let String = prompt(" please Enter a string");
+if(String==0){
+    Console.log("String is empty")
+}else{
+    console.log("string" + " " + "is" + " " + "=" + " " +  String)
+}
+
+       //Question no = 4;
+let Str = "AssIGNmeNt";
+let idx = 0;
+if(Str[idx] === Str[idx].toLowerCase()){
+    console.log("character is  lowercase")
+}else{
+    console.log("character is not lowercase")
+ }
+
+     //Question no = 5;
+let str = prompt("Please enter a string");
+console.log("original string" + " " + "=" + " " + str);
+console.log("string without spaces" + " " + "=" + " " + str.trim());
+
+      //Question no = 6;
+Arr = ["hello", 'a', 23, 64, 99, -6,-1];
+let item =23;
+if(arr.indexOf(item) != -1) {
+console.log("element exists in array");
+} else {
+console.log("element doesn't exist in array")
+}
--- Basic.js
+++ Basic.js
@@ -1,70 +1,78 @@
              //question:1
- let num = prompt("Enter a number ");
- console.log(num);
- if(num%10==0){
-    console.log("The number is divisible by 10");
- }else{
-    console.log("The number is not divisible by 10");
- }
+//  let num = prompt("Enter a number ");
+//  console.log(num);
+//  if(num%10==0){
+//     console.log("The number is divisible by 10");
+//  }else{
+//     console.log("The number is not divisible by 10");
+//  }
 
-            //question:2
-let name = prompt("Enter your name");
-let age = prompt("Enter your age");
-alert(name + " is " + age + " years old");
+//             //question:2
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(name + " is " + age + " years old");
 
-            //quetion:3
-    let quarter = 2;
-    switch(quarter){
-        case 1:
-            console.log("January , February , March");
-            break;
-        case 2:
-            console.log("April, May , June");
-            break;
-        case 3:
-            console.log("July , August , September");
-            break;
-        case 4:
-            console.log("October, November, December ");
-            break;
-        default :
-           console.log("Not a Quarter");
-           break;
-    }
+//             //quetion:3
+//     let quarter = 2;
+//     switch(quarter){
+//         case 1:
+//             console.log("January , February , March");
+//             break;
+//         case 2:
+//             console.log("April, May , June");
+//             break;
+//         case 3:
+//             console.log("July , August , September");
+//             break;
+//         case 4:
+//             console.log("October, November, December ");
+//             break;
+//         default :
+//            console.log("Not a Quarter");
+//            break;
+//     }
 
-            question:4
-    let str = "Appless";
-    if(str[0]=='A'|| str[0]=='a' && str.length>=5){
-    console.log("A string is a golden string ");
-    } else {
-        console.log("String is not a golden");
-    }
+//             question:4
+//     let str = "Appless";
+//     if(str[0]=='A'|| str[0]=='a' && str.length>=5){
+//     console.log("A string is a golden string ");
+//     } else {
+//         console.log("String is not a golden");
+//     }
 
-         //question:5
-         let a = 10;
-         let b = 15;
-         let c = 20;
-         if(a>b){
-            if(a>c){
-                console.log("a is largest");
-            }else{
-                console.log("c is largest");            
-            }
-           }else{
-            if(b>c){
-                console.log("b is largest");
-            }else{
-                console.log("c is largest");
-            }
-           }
+//          //question:5
+//          let a = 10;
+//          let b = 15;
+//          let c = 20;
+//          if(a>b){
+//             if(a>c){
+//                 console.log("a is largest");
+//             }else{
+//                 console.log("c is largest");            
+//             }
+//            }else{
+//             if(b>c){
+//                 console.log("b is largest");
+//             }else{
+//                 console.log("c is largest");
+//             }
+//            }
 
-            //question:6
-            let num1 = 52;
-            let num2 = 34562;
-            if(num1%10==num2%10){
-                console.log("Numbers have the same last digit");
-            }else{
-                console.log("Numbers don't  have the same last digit");
-            }
+//             //question:6
+//             let num1 = 52;
+//             let num2 = 34562;
+//             if(num1%10==num2%10){
+//                 console.log("Numbers have the same last digit");
+//             }else{
+//                 console.log("Numbers don't  have the same last digit");
+//             }
+                
 
-           
+    //    let  start = ["janurary" ,"july", "march","August"];
+    //         start.splice(0,2,"july","june");
+    //    console.log(start);
+       let arr = ["a","b"];
+       let arrcopy = arr;
+       arrcopy.push(c);
+
+ 
