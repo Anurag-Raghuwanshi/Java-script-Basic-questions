cb09bbc85b6221eb6d6841fa8e98b7ce78973158
Anurag Raghuwanshi <anuragraghuwanshi077@gmail.com>
2026-09-23 20:39:26 +0530

    update

--- objectliterals.js
+++ objectliterals.js
@@ -16,15 +16,15 @@ while(true){
     if(guess=="quit"){
       console.log("user quit");
       break;
-    }
-if(guess==random){
+  }
+   if(guess==random){
     console.log("you guess is right.congrats! random number was", random);
     break;
-}else{
-     guess = prompt("your guess was wrong.please try again");
+  } else if(guess<random){   
+   guess = prompt(" hint : your guess is to small.please try again");
+  } else{
+   guess = prompt("hint : your guess is to big.please try again");
+  }
+ 
 }
-}
-  // while(guess=="quit"){
-  //   console.log("user quit");
-  //   break;
-  // }if(guess==)
+  
