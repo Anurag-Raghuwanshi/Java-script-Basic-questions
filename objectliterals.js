ee9f32f4bb3da0eaf5e586da7fe00291239a1a2d
Anurag Raghuwanshi <anuragraghuwanshi077@gmail.com>
2026-09-24 15:00:09 +0530

    objectliteral

--- objectliterals.js
+++ objectliterals.js
@@ -1,30 +1,52 @@
-const Twitter ={
-    username: "0._anu._0",
-    likes : "230",
-    content : "coding",
-    tags: "mayank,satyam,neeraj",
-    repost : "om,pradeep,dishant",
-};
-console.log(Twitter);
+// const Twitter ={
+//     username: "0._anu._0",
+//     likes : "230",
+//     content : "coding",
+//     tags: "mayank,satyam,neeraj",
+//     repost : "om,pradeep,dishant",
+// };
+// console.log(Twitter);
 
 
            //guessing game 
-const  max = prompt("Enter you number");
-const random =  Math.floor(Math.random()*max)+1;
-  let guess = prompt("Enter your guess");
-while(true){
-    if(guess=="quit"){
-      console.log("user quit");
-      break;
-  }
-   if(guess==random){
-    console.log("you guess is right.congrats! random number was", random);
-    break;
-  } else if(guess<random){   
-   guess = prompt(" hint : your guess is to small.please try again");
-  } else{
-   guess = prompt("hint : your guess is to big.please try again");
-  }
+// const  max = prompt("Enter you number");
+// const random =  Math.floor(Math.random()*max)+1;
+//   let guess = prompt("Enter your guess");
+// while(true){
+//     if(guess=="quit"){
+//       console.log("user quit");
+//       break;
+//   }
+//    if(guess==random){
+//     console.log("you guess is right.congrats! random number was", random);
+//     break;
+//   } else if(guess<random){   
+//    guess = prompt(" hint : your guess is to small.please try again");
+//   } else{
+//    guess = prompt("hint : your guess is to big.please try again");
+//    }
  
-}
-  
+// }
+ 
+         //question no = 1;
+//  const dice = Math.floor(Math.random()*6)+1;
+//  console.log(dice);
+
+        //question no = 2;
+// const car = {
+//        name : ["Audi","maruti","fortuner","rangrover"],
+//        model : ["2026","2024","2016","2020"],
+//        colour : ["black","Blue","white","maroon"],
+// };
+//       console.log(car.name[0],car.model[0],car.colour[0]);
+
+
+           //question no =3;
+const person = {
+         name : "Anurag",
+         city : "Indore",
+         age  :  "23",
+};
+    person.city = "Newyork";
+    person.contry = "United State";
+    console.log(person);
